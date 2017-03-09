d4bf0a430b1043685b5831aebf025366efd1007a
jamesferry11 <jamesferry11@gmail.com>
2017-03-09 15:23:51 -0700

    completed first draft of bar graph

--- d3-bar-graph/bar-graph.js
+++ d3-bar-graph/bar-graph.js
@@ -9,7 +9,32 @@ const svg = d3.select('body')
     .attr('height', height)
     .style('padding', padding);
 
+
 d3.json('salesData.json', (data) => {
+    var xScale = d3.scaleBand()
+        .domain(data.map((d) => {
+            return d.day
+        }))
+        .range([0, width]);
+
+    var yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, (d) => {
+            return d.sales;
+        })])
+        .range([0, height]);
+
+    var xAxis = d3.axisBottom(xScale);
+
+    var yAxis = d3.axisLeft(yScale)
+        .ticks(5);
+
+    svg.append('g')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(xAxis);
+
+    svg.append('g')
+        .call(yAxis);
+
     svg.selectAll('rect')
         .data(data)
         .enter()
@@ -20,13 +45,17 @@ d3.json('salesData.json', (data) => {
         .attr('x', (d, i)=> {
             return i * (width / data.length);
         })
+
         .attr('y', (d) => {
-            return height - d.sales;
+            return height - yScale(d.sales);
         })
-        .attr('width', witdth / data.lenth - barPadding)
+
+        .attr('width', witdth / data.length - barPadding)
+
         .attr('height', (d) => {
-            return d.sales;
+            return yScale(d.sales);
         })
+
         .attr('fill', '#3399ff')
         .transition()
         .duration(2000)
@@ -48,4 +77,24 @@ d3.json('salesData.json', (data) => {
                 return '#3399ff';
             }
         });
+
+    svg.selectAll('text.label')
+        .data(data)
+        .enter()
+        .append('text')
+        .attr('class', 'label')
+        .text(function(d) {
+            return d.sales;
+        })
+        .attr('text-anchor', 'middle')
+        .attr('x', function(d, i) {
+            return i * (width / data.length) + (width / data.length - barPadding) / 2;
+        })
+        .attr('y', function(d) {
+            return yScale(d.sales) + 20;
+        })
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', '11px')
+        .attr('fill', 'white');
+
 });
